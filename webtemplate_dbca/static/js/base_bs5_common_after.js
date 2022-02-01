6edbf11da74e19b41c0b0c754023af9ee1a43d64
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-02-01 14:03:24 +0800

    Collapsing Cards with Chevron

---
+++ webtemplate_dbca/static/js/base_bs5_common_after.js
@@ -0,0 +1,29 @@
+$('.chevron-toggle').each(function(i, obj) {
+        var collapsed_id = $(this).data('bs-target');
+        var collapsed_status = $(collapsed_id).css('display');  
+        if (collapsed_status == 'none') {
+              $(this).removeClass("down-chevron-open");
+              $(this).addClass("down-chevron-close");
+
+        } else {
+              $(this).removeClass("down-chevron-close");
+              $(this).addClass("down-chevron-open");
+
+        }
+});
+
+
+$('.chevron-toggle').click(function(e) {
+       var collapsed_id = $(this).data('bs-target');
+       if ($(this).hasClass("down-chevron-close")) {
+            $(this).removeClass("down-chevron-close");
+            $(this).addClass("down-chevron-open");
+            $(collapsed_id).slideDown(500);
+
+        } else {
+
+              $(this).removeClass("down-chevron-open");
+              $(this).addClass("down-chevron-close");
+              $(collapsed_id).slideUp(500);
+        }
+})
