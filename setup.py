bc369bb0fe9515fc767982e1f6b179a9b84ba08b
Jason Moore <jmoore@austwa.com>
2022-09-02 16:59:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 install_requires = [
     'Django>=2.2',
 ]
-version = ('1.3.1')
+version = ('1.3.2')
 
 setup(
     name='webtemplate-dbca',
